f7c79a2f92f2ad94b4feffb27a8ef9f88d7aa993
lad-bhargav <ladshiv867@gmail.com>
2026-02-22 00:02:52 +0530

    delete slot done,edit route done-frontend

--- frontend/components/SlotCard.tsx
+++ frontend/components/SlotCard.tsx
@@ -1,7 +1,25 @@
+'use client'
 import { Slot } from '@/types/slot'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const SlotCard = ({id,start_time,end_time,price,date,booking_status,payment_status,email}:Slot) => {
+
+  const router = useRouter();
+
+  const deleteSlot = async() => {
+    try {
+      const res = await fetch(`http://localhost:8080/deleteslot/${id}`,{
+        method: "DELETE"
+      })
+      if(res.ok) {
+        window.location.reload()
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
         <h2>Slot ID: {id}</h2>
@@ -12,7 +30,8 @@ const SlotCard = ({id,start_time,end_time,price,date,booking_status,payment_stat
         <p>Booking Status: {booking_status}</p>
         <p>Payment Status: {payment_status}</p>
         <p>Email: {email}</p>
-        <button>delete</button>
+        <button onClick={()=> router.push(`/admin/edit/${id}`)}>edit</button>
+        <button onClick={deleteSlot}>delete</button>
     </div>
   )
 }
